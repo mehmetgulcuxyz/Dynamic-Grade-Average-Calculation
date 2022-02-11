0675a1d868a19dbdca087335b631c4900dc95b3e
mehmetgulcuxyz <mehmet.gulcu@atolyevizyon.com>
2022-02-11 18:03:48 +0300

    main

---
+++ android/app/src/main/kotlin/mehmetgulcu/xyz/grade_average_calculation/MainActivity.kt
@@ -0,0 +1,6 @@
+package mehmetgulcu.xyz.grade_average_calculation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'package:grade_average_calculation/constants/app_constants.dart';
+import 'package:grade_average_calculation/widgets/ortalama_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ortalama Hesapla',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Sabitler.anaRenk,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: OrtalamaHesapla(),
+    );
+  }
+}
